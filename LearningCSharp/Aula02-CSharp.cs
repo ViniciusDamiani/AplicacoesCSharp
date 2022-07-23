d5de76eadcb6b459572e59ea07112ccaefadf272
ViniciusDamiani <vinidamiani03@hotmail.com>
2022-07-23 11:40:45 -0300

    feat: Aula 02 - Scopo de Variaveis

---
+++ LearningCSharp/Aula02-CSharp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//
+class Aula02_CSharp
+{
+    static int num = 10;
+    static int num2 = 2;
+
+    public static void Aula02()
+    {
+       num2 += 5;
+       Console.WriteLine("Valor de num2 é: "+ num2);
+    }
+
+    static void Teste()
+    {
+       num += 5;
+        Console.WriteLine("Valor de num é: "+ num);
+    }
+
+}
+
