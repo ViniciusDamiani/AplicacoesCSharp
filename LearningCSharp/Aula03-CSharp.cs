97cd446512c5e0be7b0608e6c23bd3da7f5497be
ViniciusDamiani <vinidamiani03@hotmail.com>
2022-07-26 20:31:18 -0300

    feat: Aula 03 - Operadores e Operações

---
+++ LearningCSharp/Aula03-CSharp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Operadores e Operações
+ class Aula03_CSharp
+{
+    public static void Aula03()
+    {
+        int num = 10 + 5;
+        Console.WriteLine(num);
+
+        bool res = 10 < 5;
+        Console.WriteLine(res);
+        
+        
+        int num2 = 0;
+        num2 = num2 + 1;
+        //OU
+        num2 += 1;
+        //OU
+        num2++;
+        Console.WriteLine(num2);
+    }
+
+}
