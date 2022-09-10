6a2ded137a4acdeea9352c9acca816c939c3cebe
ViniciusDamiani <vinidamiani03@hotmail.com>
2022-09-10 11:52:20 -0300

    feat: Aula 05 - Thread

---
+++ LearningCSharp/Aula05-CSharp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Aula05_CSharp
+{ 
+    public static void Aula05()
+    {
+        static void FazerAlgo()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Thread");
+        }
+    }
+
+}
+
