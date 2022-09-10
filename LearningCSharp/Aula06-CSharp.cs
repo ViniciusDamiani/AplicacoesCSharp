0963c5cfaa26295008d714759bbb73ce3d5dccf9
ViniciusDamiani <vinidamiani03@hotmail.com>
2022-09-10 11:53:01 -0300

    feat: Aula 06 - Comentando  aplicação + Bloco Region

---
+++ LearningCSharp/Aula06-CSharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+     class Aula06_CSharp
+    {
+
+        //Metodo principal
+        public static void Aula06()
+        {
+            /* Comentando um bloco de Código */
+
+            //Comentando uma linha de Código
+
+            #region " Bloco region --> Linha de comando"
+
+            Console.WriteLine("Aula sobre organização de código!");
+            
+            #endregion    
+        }
+    }
+}
